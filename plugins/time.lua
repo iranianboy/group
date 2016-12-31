8bc60df7282a4b0848126ca5ca79745e82feb015
iranianboy <alonereza18@gmail.com>
2016-12-31 05:34:33 +0330

    Create time.lua

---
+++ plugins/time.lua
@@ -0,0 +1,10 @@
+function run(msg, matches)
+local url = http.request('http://api.gpmod.ir/time/')
+local jdat = json:decode(url)
+ local text =  '🕒 ساعت '..jdat.FAtime..' \n📆 امروز '..jdat.FAdate..' میباشد.\n    —--\n🕒 '..jdat.ENtime..'\n📆 '..jdat.ENdate.. '\n\n@Cyrusgreat'
+return text
+end
+return {
+  patterns = {"^[#/!]([Tt][iI][Mm][Ee])$"}, 
+run = run, 
+}
